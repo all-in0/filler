cff9e64dde22712bf3dfd2a0650cc9f49259d437
all-in0 <vadimuman@gmail.com>
2017-03-31 20:10:20 +0300

    h

--- my_filler.c
+++ my_filler.c
@@ -12,7 +12,8 @@
 
 #include "ft_printf/ft_printf.h"
 #include "filler.h"
-#include "stdio.h"
+#include <stdlib.h>
+
 
 int 	try_paste(t_fil *ph, int i)
 {
@@ -29,7 +30,7 @@ int 	try_paste(t_fil *ph, int i)
 			 ph->fig[b] == '*') || ph->map[i] == '\n')
 			return (0);
 		if (ph->fig[b] == '\n')
-			i = i + ph->fig_x + ph->x + 1;
+			i = i + ph->x - ph->fig_x + 1;
 		else
 			i++;
 	}
@@ -52,11 +53,12 @@ int 	check_koef(t_fil *ph, int i)
 		if (ph->fig[b] == '*') {
 			a++;
 			c = c + ph->imap[a];
+		//	ft_printf("%d\n", ph->imap[a]);
 		}
 		else if (ph->fig[b] == '.')
 			a++;
 		else if (ph->fig[b] == '\n')
-			a = a + ph->fig_x + ph->x + 1;
+			a = a - ph->fig_x + ph->x + 1;
 	}
 	if (c < ph->co || ph->co == 0)
 	{
@@ -72,20 +74,20 @@ int		make_koef(t_fil *ph)
 	int		n;
 
 	ph->imap = (int *)malloc(sizeof(ph->imap) * ph->x * ph->y + ph->y);
-	n = 15;
+	n = 1;
 	i = -1;
 	while (ph->map[++i] > '\0')
 		if (ph->map[i] == ph->sym4 || ph->map[i] == ph->sym3) {
 			ph->imap[i] = n;
 		}
-	while (--n > 5)
+	while (++n < 50)
 	{
 		i = -1;
 		while (ph->map[++i] != '\0') {
-			if (((ph->imap[i + 1] == (n + 1) || ph->imap[i - 1] == (n + 1) ||
-				ph->imap[i + ph->x] == (n + 1) || ph->imap[i + ph->x + 1] == (n + 1) ||
-				ph->imap[i + ph->x + 2] == (n + 1) || ph->imap[i - ph->x] == (n + 1) ||
-				ph->imap[i - ph->x - 1] == (n + 1) || ph->imap[i - ph->x - 2] == (n + 1)) && ph->imap[i] < (n + 1)) && ph->imap[i] == 0)
+			if (((ph->imap[i + 1] == (n - 1) || ph->imap[i - 1] == (n - 1) ||
+				ph->imap[i + ph->x] == (n - 1) || ph->imap[i + ph->x + 1] == (n - 1) ||
+				ph->imap[i + ph->x + 2] == (n - 1) || ph->imap[i - ph->x] == (n - 1) ||
+				ph->imap[i - ph->x - 1] == (n - 1) || ph->imap[i - ph->x - 2] == (n - 1)) && ph->imap[i] < (n - 1)) && ph->imap[i] == 0)
 				ph->imap[i] = n;
 		}
 	}
@@ -93,14 +95,10 @@ int		make_koef(t_fil *ph)
 
 bool 	find_place(t_fil *phars) {
 	int i;
-	int a;
-	int b;
 
 	i = -1;
-	a = 0;
-	b = 0;
 	make_koef(phars);
-	while (++i <= (phars->x * (phars->y + 1)) - phars->fig_x * (phars->fig_y + 1))
+	while (++i <= (phars->x * (phars->y + 1)) - (phars->x * phars->fig_y + (phars->x + 1)))
 		if (try_paste(phars, i))
 			check_koef(phars, i);
 	if (phars->co == 0)
@@ -115,8 +113,6 @@ bool	print_fig(t_fil *phar)
 
 	a = phar->find_plase / (phar->x + 1);
 	b = phar->find_plase % (phar->x + 1);
-//	phars->map[i] = 'U';
-//	ft_printf("%s", phars->map);
 	ft_printf("%d %d\n", a, b);
 	return (true);
 }
@@ -128,6 +124,8 @@ int 	save_fig(t_fil *ph, int fd)
 	int 	b;
 	char	*line;
 
+	if (ph->fig != NULL)
+		free(ph->fig);
 	b = -1;
 	a = -1;
 	get_next_line(fd, &line);
@@ -152,6 +150,8 @@ int		save_map(t_fil *phars, int fd)
 	int		i;
 	int		b;
 
+	if (phars->map != NULL)
+		free(phars->map);
 	a = -1;
 	b = -1;
 	get_next_line(fd, &line);
@@ -159,7 +159,7 @@ int		save_map(t_fil *phars, int fd)
 	phars->x = ft_atoi(&line[11]);
 	if (phars->y == 0 && phars->x == 0)
 		return (0);
-	phars->map = (char *)malloc(sizeof(phars->map) * (phars->x * phars->y + phars->y + 1));
+	phars->map = (char *)malloc(sizeof(phars->map) * (phars->x * phars->y + phars->y));
 	get_next_line(fd, &line);
 	while (++b < phars->y && get_next_line(fd, &line))
 	{
@@ -176,16 +176,15 @@ int		save_map(t_fil *phars, int fd)
 		phars->map[++a] = '\n';
 	}
 	phars->map[a] = '\0';
+	return (1);
 }
 
 int		save_all(t_fil *phars, int fd)
 {
 	char *line;
 
-	while (!ft_strstr(line, "a.out")) {
-		get_next_line(fd, &line);
-		phars->pl = line[10] - '0';
-	}
+	get_next_line(fd, &line);
+	phars->pl = line[10] - '0';
 	if (phars->pl == 1)
 	{
 		phars->sym = 'O';
@@ -208,25 +207,25 @@ int		main(int argc, char **argv)
 	t_fil phars;
 	bool pr;
 	int fd;
-	int fd1;
-	//FILE *f = fopen("file1.txt", "w");
 
-
-	fd = open("/nfs/2016/v/vnakonec/filler/cmake-build-debug/file.txt", O_RDONLY);
-	//fd = 0;
+	//fd = open("/nfs/2016/v/vnakonec/filler/cmake-build-debug/file.txt", O_RDONLY);
+	fd = 0;
 	pr = 1;
 	__builtin_bzero(&phars, sizeof(phars));
 	save_all(&phars, fd);
 	while (pr == true)
 	{
+		phars.co = 0;
 	if	(!save_map(&phars, fd) || !save_fig(&phars, fd))
 	{
 		ft_printf("0 0\n");
 	}
-	pr = find_place(&phars);
-	print_fig(&phars);
-	free(phars.map);
-	free(phars.fig);
+	else
+	{
+		pr = find_place(&phars);
+		print_fig(&phars);
+	}
+		__builtin_bzero(phars.imap, sizeof(phars.imap));
 		phars.y = 0;
 		phars.x = 0;
 	}
